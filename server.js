20d39aa60c7f0d6234ea8e0b823f01623ec3cb94
Dario Kemenovic <dkemenovic@gmail.com>
2019-03-25 03:07:03 +0100

    Complete homework-9

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 let http = require("http");
+let halls = [];
+let hallId = 0;
+let hallNames = [];
+
+function add(hallName) {
+  let hallObject = {
+    id: hallId,
+    name: hallName,
+    reservation: {
+      isReserved: false,
+      reservedFrom: null,
+      reservedUntil: null
+    }
+  }
+
+  halls.push(hallObject);
+  hallId++;
+}
+
+add('Klub Kulture');
+add('Hrvatski Dom');
+add('Vladimir Nazor');
+add('Galerija K1');
+add('Ljudevit Modec');
+
+hallNames = halls.map(function (hall) {
+    return hall.name;
+})
+
+let stringResponse = hallNames.join(', ');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("Hello world!");
+    res.write(stringResponse);
     res.end();
-}).listen(8000);
+}).listen(8001);
 
-console.log("server running at http://127.0.0.1:8000");
+console.log("server running at http://127.0.0.1:8001");
 
