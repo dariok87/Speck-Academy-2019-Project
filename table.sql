c12b5c8e4a5678e60dc392417326824874e93575
Dario Kemenovic <dkemenovic@gmail.com>
2019-04-20 23:00:14 +0200

    Create sql tables

---
+++ table.sql
@@ -0,0 +1,43 @@
+
+--halls
+
+CREATE TABLE halls(
+   hall_id SERIAL PRIMARY KEY,
+   name TEXT NOT NULL,
+   address VARCHAR(50),
+   reservation TEXT NOT NULL,        
+   capacity INTEGER
+);
+
+
+
+--users
+
+CREATE TABLE users(
+   user_id SERIAL PRIMARY KEY,
+   username TEXT NOT NULL,
+   email VARCHAR(50),
+   users_password VARCHAR(50) NOT NULL,
+   status TEXT NOT NULL
+);
+
+
+
+--reservations
+
+CREATE TABLE reservations(
+   reservation_id SERIAL PRIMARY KEY,
+   is_reserved INTEGER,
+   reservation_start TIMESTAMP,
+   reservation_end TIMESTAMP,
+   hall_id INTEGER REFERENCES halls (hall_id)  
+);
+
+
+--sessions
+
+CREATE TABLE sessions(
+   session_id SERIAL PRIMARY KEY,
+   user_id INTEGER REFERENCES users (user_id)  
+);
+
